140ef2972b0260c5512c23120d55c62d326c0f5b
prajjhu <prajjhu@icloud.com>
2026-03-28 02:53:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,18 +13,20 @@ TARGET_WINDOW = 20
 WARNING_DECAY = 35
 CONTEXT_LIMIT = 3
 
-BANNED = ["nigger","faggot","rape","pedophile","nazi","hitler","heil","childporn","kanker"]
-TRIGGERS = ["fuck you", "kys", "kill yourself", "worthless", "nobody likes you", "shut up", "stfu", "retard"]
+BANNED = ["nigger", "faggot", "rape", "pedophile", "nazi", "hitler", "heil", "childporn", "kanker"]
+TRIGGERS = ["kys", "kill yourself", "worthless", "nobody likes you", "shut up"]
 
 DIRECT_TARGET_PHRASES = [
-    "fuck you", "you are", "you're", "ur ", "u are", "shut up", "stfu",
     "nobody likes you", "worthless", "go cry", "cry more"
 ]
+
 LIGHT_HOSTILITY_WORDS = ["idiot", "stupid", "trash", "bitch", "loser", "dumb", "retard"]
+
 SEVERE_TARGET_PHRASES = [
     "kill yourself", "kys", "go die", "i will kill you", "i'll kill you",
     "rape you", "pedophile", "nigger", "faggot"
 ]
+
 EXIT_PHRASES = [
     "i'm done", "im done", "done with this", "ain't talking to you again",
     "not talking to you again", "leave me alone", "goodbye", "bye", "end of convo"
